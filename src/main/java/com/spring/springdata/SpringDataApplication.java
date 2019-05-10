0423221dd914e05b58f03d60965bcfcf1174b585
Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>
2019-05-10 10:19:10 +0300

    'init'

---
+++ src/main/java/com/spring/springdata/SpringDataApplication.java
@@ -0,0 +1,10 @@
+package com.spring.springdata;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+public class SpringDataApplication {
+    public static void main(String[] args) {
+
+    }
+}
